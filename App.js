e984f8c85c64d920dfe5e32df8ae8666f03860b8
lima8002 <lima8002@gmail.com>
2020-09-16 19:09:19 +1000

    Add textInput and button

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, TextInput, SafeAreaView, TouchableOpacity, FlatList } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView>
+      <Text style={styles.title}>EzList</Text>
+      <TextInput style={styles.textInput} placeholder='insert your task' />
+      <TouchableOpacity style={styles.addButton}>
+        <Text>Add</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
-    </View>
-  );
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +22,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  title:{
+    padding:10,
+    fontSize: 30,
+  },
+  textInput: {
+    marginHorizontal: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+    
+  }, 
+  addButton: {
+    alignItems: 'center',
+    backgroundColor:'lightblue', 
+    padding:10,
+    marginHorizontal:80,
+    marginTop:10,
+    borderRadius:5,
+  }
+})
